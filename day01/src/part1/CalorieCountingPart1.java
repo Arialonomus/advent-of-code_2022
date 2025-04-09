193f6a01eccd8b33d4d7b7fa372d9b700fdc94f3
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-09 16:07:20 -0500

    Implemented parsing and summing solution using streams, solved Part 1

--- day01/src/part1/CalorieCountingPart1.java
+++ day01/src/part1/CalorieCountingPart1.java
@@ -1,7 +1,35 @@
 package part1;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static java.lang.System.Logger.Level.*;
+
 public class CalorieCountingPart1 {
     public static void main(String[] args) {
+        // Assumes the working directory is this day's module
+        Path input_file_path = Path.of("./" + args[0]);
 
+        // Calories carried by each elf are delimited by blank lines
+        try (Stream<String> groups = Stream.of(Files.readString(input_file_path).split("\\n\\n"))) {
+            int highest_calories = groups
+                    .mapToInt(
+                            // Each entry in the string stream represents the calories carried by a single elf
+                            s -> Stream.of(s.split("\n"))
+                            .mapToInt(Integer::parseInt)
+                            .sum())
+                    .max()
+                    .orElse(0); // Handle empty cases
+
+            // Display puzzle solution
+            System.out.println(highest_calories);
+        }
+        catch (IOException | UncheckedIOException e) {  // Streams may throw UncheckedIOExceptions while fetching
+            System.Logger logger = System.getLogger(CalorieCountingPart1.class.getName());
+            logger.log(WARNING, "Error reading input file ", input_file_path, e);
+        }
     }
 }
