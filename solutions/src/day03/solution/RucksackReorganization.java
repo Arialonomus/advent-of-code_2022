98cf635e0d11fd731a9d4d504f7913a508c4b482
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-10 15:41:26 -0500

    Refactored solution to handle different grouping requirements for each part. Solved Part 2

--- solutions/src/day03/solution/RucksackReorganization.java
+++ solutions/src/day03/solution/RucksackReorganization.java
@@ -2,22 +2,31 @@ package day03.solution;
 
 import enums.Part;
 import interfaces.AOCSolution;
+import utils.ListUtils;
 import utils.StringUtils;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static java.lang.System.Logger.Level.WARNING;
 
 public class RucksackReorganization implements AOCSolution {
+    public static final int PARTITION_SIZE = 3;
+
     @Override
     public String solve(Part puzzle_part, Path input_file_path, System.Logger logger) {
         try (Stream<String> rucksacks = Files.lines(input_file_path)) {
-            int priority_sum = rucksacks
-                    .map(StringUtils::bisect)
+            List<List<String>> rucksack_groups = puzzle_part == Part.PART_1 ?
+                    // Part 1 requires each rucksack be partitioned into two groups
+                    rucksacks.map(StringUtils::bisect).toList() :
+                    // Part 2 requires every 3 rucksacks be grouped
+                    ListUtils.partition(rucksacks.toList(), PARTITION_SIZE);
+
+            int priority_sum = rucksack_groups.stream()
                     .map(StringUtils::intersection)
                     .map(str -> str.charAt(0))
                     // Map [a-z] to [1-26] and [A-Z] to [27-52]
