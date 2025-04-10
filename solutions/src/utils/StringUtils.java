6eec2b21459ae4704b5fa9b89731af4e716bc74d
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-10 13:54:34 -0500

    Added toCharSet() method for processing strings to sets for use in other methods

--- solutions/src/utils/StringUtils.java
+++ solutions/src/utils/StringUtils.java
@@ -1,7 +1,10 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StringUtils {
     /**
@@ -16,5 +19,16 @@ public class StringUtils {
 
         int middle = str.length() / 2;
         return new ArrayList<>(List.of(str.substring(0, middle), str.substring(middle)));
+    }
+
+    /**
+     * Converts a string to a Set of Characters, for use in processing strings
+     * @param str the string to be converted
+     * @return a Set of unique characters found in the passed-in string
+     */
+    public static Set<Character> toCharSet(String str) {
+        return str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toSet());
     }
 }
