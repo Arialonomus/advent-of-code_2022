80e9929950dbc2a854a82b9ef834f5a9f69ab73d
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-10 12:08:38 -0500

    Moved Day 2 Part 2 solution files into solutions module

---
+++ solutions/src/day02/solution/RockPaperScissors.java
@@ -0,0 +1,37 @@
+package part2;
+
+import utils.RoundResolver;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static java.lang.System.Logger.Level.WARNING;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        System.Logger logger = System.getLogger(RockPaperScissors.class.getName());
+
+        // Assumes the working directory is this day's module
+        Path input_file_path = Path.of("./" + args[0]);
+
+        // Initialize the Round Resolver
+        RoundResolver resolver = new RoundResolver(RoundResolver.Part.PART_2);
+
+        // Each round should be resolved according to some scoring rule
+        try (Stream<String> rounds = Files.lines(input_file_path)) {
+            int total_score = rounds
+                    .mapToInt(resolver::getScore)
+                    .sum();
+
+            System.out.println(total_score);
+        }
+        catch (IOException | UncheckedIOException e) {  // Streams may throw UncheckedIOExceptions while fetching
+            logger.log(WARNING, "Error reading input file ", input_file_path, e);
+        } catch (IllegalArgumentException e) {
+            logger.log(WARNING, "Invalid input file format ", input_file_path, e);
+        }
+    }
+}
---
+++ solutions/src/day02/utils/RoundResolver.java
@@ -0,0 +1,52 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static utils.Constants.*;
+
+public class RoundResolver {
+    public enum Part{ PART_1, PART_2 }
+    private final Map<Character, Map<Character, Integer>> scoring_map;
+
+    public RoundResolver(Part puzzle_part){
+        // Define the sub-mappings for round outcomes
+        Map<Character, Integer> rock = new HashMap<>();
+        rock.put('X', puzzle_part == Part.PART_1 ? ROCK + DRAW : LOSE + SCISSORS);
+        rock.put('Y', puzzle_part == Part.PART_1 ? PAPER + WIN : DRAW + ROCK);
+        rock.put('Z', puzzle_part == Part.PART_1 ? SCISSORS + LOSE : WIN + PAPER);
+
+        Map<Character, Integer> paper = new HashMap<>();
+        paper.put('X', ROCK + LOSE);
+        paper.put('Y', PAPER + DRAW);
+        paper.put('Z', SCISSORS + WIN);
+
+        Map<Character, Integer> scissors = new HashMap<>();
+        scissors.put('X', puzzle_part == Part.PART_1 ? ROCK + WIN : LOSE + PAPER);
+        scissors.put('Y', puzzle_part == Part.PART_1 ? PAPER + LOSE : DRAW + SCISSORS);
+        scissors.put('Z', puzzle_part == Part.PART_1 ? SCISSORS + DRAW : WIN + ROCK);
+
+        // Build the nested hashmap
+        scoring_map = new HashMap<>();
+        scoring_map.put('A', rock);
+        scoring_map.put('B', paper);
+        scoring_map.put('C', scissors);
+    }
+
+    public int getScore(String round) {
+        // Read in round moves
+        if (round.length() != 3)
+            throw new IllegalArgumentException("Rounds must only be 3 characters long. Round provided: " + round);
+
+        char opponent_move = round.charAt(0);
+        if ("ABC".indexOf(opponent_move) < 0)
+            throw new IllegalArgumentException("Opponent move must be 'A', 'B', or 'C'. Round provided: " + round);
+
+        char player_move = round.charAt(2);
+        if ("XYZ".indexOf(player_move) < 0)
+            throw new IllegalArgumentException("Player move must be 'X', 'Y', 'Z'. Round provided: " + round);
+
+        // Calculate the score based on the mapped outcomes
+        return scoring_map.get(opponent_move).get(player_move);
+    }
+}
