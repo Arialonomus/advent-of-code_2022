c5c8a963e7db0dcf6a72f5c0e0cbf148c9c0c961
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 18:18:02 -0500

    Added custom toString() implementation

--- solutions/src/enums/Part.java
+++ solutions/src/enums/Part.java
@@ -1,3 +1,17 @@
 package enums;
 
-public enum Part{ PART_1, PART_2 }
+public enum Part{
+    PART_1("Part 1"),
+    PART_2("Part 2");
+
+    private final String render_name;
+
+    Part(String render_name) {
+        this.render_name = render_name;
+    }
+
+    @Override
+    public String toString() {
+        return render_name;
+    }
+}
