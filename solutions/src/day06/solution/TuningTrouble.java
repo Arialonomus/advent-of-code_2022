ec1be9102e15c3419e7b13ff60304315bd714716
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 14:38:46 -0500

    Implemented simple test using a set of characters to check for the start of message marker. Solved Part 1

--- solutions/src/day06/solution/TuningTrouble.java
+++ solutions/src/day06/solution/TuningTrouble.java
@@ -14,6 +14,23 @@ import static java.lang.System.Logger.Level.WARNING;
 public class TuningTrouble implements AOCSolution {
     @Override
     public String solve(Part puzzle_part, Path input_file_path, System.Logger logger) {
+        try {
+            CharSequence characters = Files.readString(input_file_path);
+            int marker_end = 3;
+            Set<Character> marker_characters = new HashSet<>(4);
+            while (marker_characters.size() < 4) {
+                marker_end++;
+                marker_characters.clear();
+                for (int i = marker_end - 4; i < marker_end; i++) {
+                    marker_characters.add(characters.charAt(i));
+                }
+            }
+
+            return String.valueOf(marker_end);
+        }
+        catch (IOException e) {
+            logger.log(WARNING, "Error reading input file ", input_file_path, e);
+        }
 
         return "ERROR";
     }
