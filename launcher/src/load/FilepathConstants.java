c163b0d4a1d20da12ae295a5bf426b4b541a9bd4
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 17:50:14 -0500

    Defined named constants for relative file paths used in solution loading

---
+++ launcher/src/load/FilepathConstants.java
@@ -0,0 +1,7 @@
+package load;
+
+public class FilepathConstants {
+    public static final String INPUT_DIR = "solutions/input/";
+
+    public static final String COMPILED_SOLUTIONS_MODULE_DIR = "out/production/solutions/";
+}
