a32332c5b698150457863ac013dbbd31ed6cc714
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 15:33:19 -0500

    Added parsing the name of the puzzle for the result output

--- launcher/src/program/AOCSolutionLauncher.java
+++ launcher/src/program/AOCSolutionLauncher.java
@@ -51,9 +51,12 @@ public class AOCSolutionLauncher {
             String result = solution.solve(loader_args.puzzle_part(), loader_args.input_file_path(), logger);
 
             // Output result
-            String day_str = loader_args.puzzle_day().substring(4);
+            String day_str = loader_args.puzzle_day().replaceAll("day0?", "");
+            String puzzle_name = solution_class_name
+                    .substring(solution_class_name.lastIndexOf('.') + 1)
+                    .replaceAll("(?<!^)([A-Z])", " $1");
             String part_str = loader_args.puzzle_part() == Part.PART_1 ? "Part 1" : "Part 2";
-            System.out.println("Day " + day_str + " (" + part_str + ") result: " + result);
+            System.out.println("Day " + day_str + " - " + puzzle_name + " (" + part_str + ") Solution: " + result);
 
         } catch (IllegalArgumentException e) {
             logger.log(WARNING, "Error parsing solver arguments: ", e);
