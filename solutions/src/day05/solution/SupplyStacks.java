51ecf42aa8bfb091c3208243cc3650e620821b46
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 10:37:55 -0500

    Added logic to instantiate a new CrateStacks object

--- solutions/src/day05/solution/SupplyStacks.java
+++ solutions/src/day05/solution/SupplyStacks.java
@@ -1,13 +1,30 @@
 package day05.solution;
 
+import day05.utils.CrateStacks;
 import enums.Part;
 import interfaces.AOCSolution;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+
+import static java.lang.System.Logger.Level.WARNING;
 
 public class SupplyStacks implements AOCSolution {
     @Override
     public String solve(Part puzzle_part, Path input_file_path, System.Logger logger) {
+        try {
+            List<String> input = Files.readAllLines(input_file_path);
+            int div_index = input.indexOf("");  // Input divided into sections by blank line
+
+            // Populate the content of the stacks
+            int num_stacks = input.get(div_index - 1).trim().split("\\s+").length;
+            List<String> stacks_input = input.subList(0, div_index - 1);
+            CrateStacks stacks = new CrateStacks(num_stacks, stacks_input);
+        } catch (IOException e) {
+            logger.log(WARNING, "Error reading input file ", input_file_path, e);
+        }
 
         return "ERROR";
     }
