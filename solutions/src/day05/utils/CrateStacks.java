a7f787cf8872e23d23769d4c6b9cfa98d49e32cf
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 11:07:57 -0500

    Added moveCrates() method to execute a single move instruction on the crate stacks

--- solutions/src/day05/utils/CrateStacks.java
+++ solutions/src/day05/utils/CrateStacks.java
@@ -3,7 +3,7 @@ package day05.utils;
 import java.util.List;
 
 public class CrateStacks {
-    private StringBuilder[] stacks;
+    private final StringBuilder[] stacks;
 
     public CrateStacks(int num_stacks, List<String> input) {
         stacks = new StringBuilder[num_stacks];
@@ -22,5 +22,25 @@ public class CrateStacks {
                 stack_index++;
             }
         }
+    }
+
+    /**
+     * Simulates moving crates one at a time from one stack to another, updating the class state
+     * @param num_crates the number of crates to be moved
+     * @param from_ID the ID number (ID = index + 1) of the stack to be moved from
+     * @param to_ID the ID number (ID = index + 1) of the stack to be moved to
+     */
+    public void moveCrates(int num_crates, int from_ID, int to_ID) {
+        // Get the substring representing the stack of crates to be moved
+        StringBuilder from_stack = stacks[from_ID - 1];
+        int stack_move_index = from_stack.length() - num_crates;
+        StringBuilder crates = new StringBuilder(from_stack.substring(stack_move_index));
+
+        // Append in reverse to simulate crates being moved one at a time
+        StringBuilder to_stack = stacks[to_ID - 1];
+        to_stack.append(crates.reverse());
+
+        // Trim the characters from the source stack to represent the crates being removed
+        from_stack.delete(stack_move_index, from_stack.length());
     }
 }
