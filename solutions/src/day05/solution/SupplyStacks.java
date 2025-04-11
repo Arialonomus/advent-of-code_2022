5ac757a1c01197098600a102b1b3a645655bab02
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-11 11:11:47 -0500

    Added code to parse input into integer arrays representing move instructions

--- solutions/src/day05/solution/SupplyStacks.java
+++ solutions/src/day05/solution/SupplyStacks.java
@@ -7,6 +7,7 @@ import interfaces.AOCSolution;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.System.Logger.Level.WARNING;
@@ -22,6 +23,15 @@ public class SupplyStacks implements AOCSolution {
             int num_stacks = input.get(div_index - 1).trim().split("\\s+").length;
             List<String> stacks_input = input.subList(0, div_index - 1);
             CrateStacks stacks = new CrateStacks(num_stacks, stacks_input);
+
+            // Parse the moves into arrays of integers
+            List<int[]> instructions = input.subList(div_index + 1, input.size()).stream()
+                    .map(str -> str.substring(5).split(" from | to "))
+                    .map(array -> Arrays.stream(array)
+                            .mapToInt(Integer::parseInt)
+                            .toArray())
+                    .toList();
+
         } catch (IOException e) {
             logger.log(WARNING, "Error reading input file ", input_file_path, e);
         }
