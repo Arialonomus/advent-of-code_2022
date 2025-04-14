3f5055be0bca9b6af2e4a93cc9474f9eb5dc0c6e
Jacob Barber <barber.jacobervin@gmail.com>
2025-04-14 15:53:24 -0500

    Added parsing of input lines into List of KnotMoves

--- solutions/src/day09/solution/RopeBridge.java
+++ solutions/src/day09/solution/RopeBridge.java
@@ -1,13 +1,41 @@
 package day09.solution;
 
+import day09.utils.KnottedRope;
+import enums.Direction;
 import enums.Part;
 import interfaces.AOCSolution;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static enums.Part.PART_1;
+import static java.lang.System.Logger.Level.WARNING;
 
 public class RopeBridge implements AOCSolution {
+    record KnotMove(Direction dir, int distance) {}
+
     @Override
     public String solve(Part puzzle_part, Path input_file_path, System.Logger logger) {
+        try (Stream<String> lines = Files.lines(input_file_path)) {
+            List<KnotMove> moves = lines
+                    .map(str -> str.split(" "))
+                    .map(pair -> {
+                        Direction dir = switch (pair[0]) {
+                            case "U" -> Direction.UP;
+                            case "D" -> Direction.DOWN;
+                            case "L" -> Direction.LEFT;
+                            default -> Direction.RIGHT;
+                        };
+                        return new KnotMove(dir, Integer.parseInt(pair[1]));
+                    })
+                    .toList();
+        }
+        catch (IOException e) {
+            logger.log(WARNING, "Error reading input file ", input_file_path, e);
+        }
 
         return "ERROR";
     }
